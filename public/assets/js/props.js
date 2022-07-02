c3a7c202a10c88b31aa10c519ebbfb45c5b2d3a0
Omar Mejenes <ok_mejenes_249@hotmail.com>
2022-07-02 17:02:43 -0500

    fixes

---
+++ public/assets/js/props.js
@@ -0,0 +1,26 @@
+[
+    {
+      "instance_id": "17c24bf453e14dd3a815772cdf68cf9c",
+      "instance_jwt": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpbnN0YW5jZUlkIjoiMTdjMjRiZjQ1M2UxNGRkM2E4MTU3NzJjZGY2OGNmOWMiLCJicmFuZCI6Imhvc3RnYXRvcl9tZXhpY28iLCJleHAiOjE2NTY4ODU2MTB9.akD1egrW0aNdm8_o2b-tiir8dPWm32TJK6Qa3VXxHfg",
+      "payment_plan_id": null,
+      "element_id": null,
+      "property_values": null,
+      "property_definitions": null,
+      "url": "https://in-app.mywebsitebuilder.com/sdk-insights-tracker?appMarketEnv=prod&debug=true&instanceJwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpbnN0YW5jZUlkIjoiMTdjMjRiZjQ1M2UxNGRkM2E4MTU3NzJjZGY2OGNmOWMiLCJicmFuZCI6Imhvc3RnYXRvcl9tZXhpY28iLCJleHAiOjE2NTY4ODU2MTB9.akD1egrW0aNdm8_o2b-tiir8dPWm32TJK6Qa3VXxHfg",
+      "sku": "AM_INSIGHTS",
+      "setup_status": 0,
+      "settings": null
+    },
+    {
+      "instance_id": "a8be8e5ff4da4e5fb14886658e3398c5",
+      "instance_jwt": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpbnN0YW5jZUlkIjoiYThiZThlNWZmNGRhNGU1ZmIxNDg4NjY1OGUzMzk4YzUiLCJicmFuZCI6Imhvc3RnYXRvcl9tZXhpY28iLCJleHAiOjE2NTY4ODU2MTB9.wNC7UiPtlh4aH5N5vwlpls8hMOVdf-P0-qmw9Ib1umc",
+      "payment_plan_id": "",
+      "element_id": null,
+      "property_values": null,
+      "property_definitions": null,
+      "url": "https://st-shop.mywebsitebuilder.com/shopping-cart-sdk.js?instanceJwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpbnN0YW5jZUlkIjoiYThiZThlNWZmNGRhNGU1ZmIxNDg4NjY1OGUzMzk4YzUiLCJicmFuZCI6Imhvc3RnYXRvcl9tZXhpY28iLCJleHAiOjE2NTY4ODU2MTB9.wNC7UiPtlh4aH5N5vwlpls8hMOVdf-P0-qmw9Ib1umc",
+      "sku": "STORE20",
+      "setup_status": 0,
+      "settings": null
+    }
+  ]
